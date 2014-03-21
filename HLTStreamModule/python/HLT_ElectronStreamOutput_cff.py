ce72ea74b9298d211006ee8bb683083847605678
Raffaele <raffaele.gerosa@cern.ch>
2014-03-21 17:14:26 +0100

    news and fixes

--- HLTStreamModule/python/HLT_ElectronStreamOutput_cff.py
+++ HLTStreamModule/python/HLT_ElectronStreamOutput_cff.py
@@ -21,7 +21,7 @@ StremSelectedEvents =  cms.vstring('HLT_Ele25_WP70_v13',
 			            ) 
 				    
 StremSelectedEventsGsf = cms.vstring(#'HLT_GsfEle25_WP70_v13',
- 	 			     'HLT_GsfEle25_WP80_v13',
+ 	 			     #'HLT_GsfEle25_WP80_v13',
   			             #'HLT_GsfEle25_WP90_v13',
 				     #'HLT_GsfEle25_WP70_PFMET_MT50_v9',
 				     'HLT_GsfEle25_WP80_PFMET_MT50_v9',
@@ -68,24 +68,25 @@ else:
                                            'keep *_hltL1GtObjectMap_*_*',
                                            'keep triggerTriggerEvent_*_*_*',
 	  				   'keep *_HLTselectedElectronFEDList_*StremElectronRawFedData*_*',
-#                                           'keep *_*hltKT6PFJets_rho_*' ,
-#                                           'keep *_*hltGsfEleAnyL1SeededElectronTrackIso*_*_*',
-#                                           'keep *_*hltL1SeededGsfTrackVars*_*_*',
-#                                           'keep *_*hltL1SeededPhotonEcalIso*_*_*',
-#                                           'keep *_*hltL1SeededPhotonHcalIso*_*_*',
-#                                           'keep *_*hltL1SeededPhotonHcalForHE*_*_*',
-#                                           'keep *_*hltPFMETProducer*_*_*',
-#                                           'keep *_*hltFastPVPixelVertices*_*_*'
+                                           'keep *_*hltKT6PFJets_rho_*' ,
+                                           'keep *_*hltGsfEleAnyL1SeededElectronTrackIso*_*_*',
+                                           'keep *_*hltL1SeededGsfTrackVars*_*_*',
+                                           'keep *_*hltL1SeededPhotonEcalIso*_*_*',
+                                           'keep *_*hltL1SeededPhotonHcalIso*_*_*',
+                                           'keep *_*hltL1SeededPhotonHcalForHE*_*_*',
+                                           'keep *_*hltPFMETProducer*_*_*',
+                                           'keep *_*hltFastPVPixelVertices*_*_*'
 					  ]
  
 
 HLTselectedElectronFEDList = cms.EDProducer("selectedElectronFEDListProducerv2",
-	electronCollections = cms.VInputTag('hltEle25WP80PFMT50PFMTFilter'),
+	electronCollections = cms.VInputTag('hltGsfEle25WP80PFMT50PFMTFilter'),
 	isGsfElectronCollection = cms.vint32(True), 
 	beamSpot            = cms.InputTag("hltOnlineBeamSpot"),
-        hcalTowersTag       = cms.InputTag("hltTowerMakerForAll"),                
+        HBHERecHitCollection = cms.InputTag("hltHbhereco"),                
 	ESLookupTable       = cms.untracked.FileInPath('EventFilter/ESDigiToRaw/data/ES_lookup_table.dat'),
 	dRStripRegion       = cms.double(0.3),
+	dRHcalRegion        = cms.double(0.5),
 	dPhiPixelRegion     = cms.double(0.3),
 	dEtaPixelRegion     = cms.double(0.3),
 	maxZPixelRegion     = cms.double(24.),
@@ -94,10 +95,11 @@ HLTselectedElectronFEDList = cms.EDProducer("selectedElectronFEDListProducerv2",
 	dumpSelectedEcalFed     = cms.bool(True),
 	dumpSelectedSiStripFed  = cms.bool(True),
 	dumpSelectedSiPixelFed  = cms.bool(True),
-	dumpSelectedHCALFed     = cms.bool(True),
+	dumpSelectedHCALFed     = cms.bool(False),
 	dumpAllEcalFed    = cms.bool(False),
 	dumpAllTrackerFed = cms.bool(False),
 	dumpAllHcalFed    = cms.bool(False),
         addThisSelectedFEDs = cms.vint32(812,813),                                     
-	debug = cms.bool(False),
+	debug = cms.bool(True)
 )
+
